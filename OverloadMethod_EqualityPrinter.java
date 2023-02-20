948ea75e8fdaa240cf40f76d083580d2cb0fecd0
Edwin Calderon <calderonedwin3@gmail.com>
2023-02-19 23:15:45 -0600

    Updating EqualityPrinter Work

--- OverloadMethod_EqualityPrinter.java
+++ OverloadMethod_EqualityPrinter.java
@@ -1,3 +1,20 @@
+/* Write a method printEqual with 3 parameters of type int. The method should not return anything (void).
+If one of the parameters is less than 0, print text "Invalid Value".
+If all numbers are equal print text "All numbers are equal"
+If all numbers are different print text "All numbers are different".
+Otherwise, print "Neither all are equal or different".
+*/
 public class OverloadMethod_EqualityPrinter {
-    // testing comments
+    public static void main(String[] args) {
+    }
+    public static void printEqual(int x, int y, int z){
+        if (x < 0 || y < 0 || z < 0)
+            System.out.println("Invalid Value");
+        else if (x == y && x == z)
+            System.out.println("All numbers are equal");
+        else if (x != y && y != z && x != z)
+            System.out.println("All numbers are different");
+        else
+            System.out.println("Neither all are equal or different");
+    }
 }
