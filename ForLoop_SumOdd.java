109159c761d07cc9ef12fcfcb85bfbebea4317cb
Edwin Calderon <calderonedwin3@gmail.com>
2023-02-20 19:47:11 -0600

    Adding ForLoop Sum Odd Work

---
+++ ForLoop_SumOdd.java
@@ -0,0 +1,34 @@
+/*
+Write a method called isOdd with an int parameter and call it number. The method needs to return a boolean.
+Check that number is > 0, if it is not return false.
+If number is odd return true, otherwise return false.
+Write a second method called sumOdd that has 2 int parameters start and end, which represent a range of numbers.
+The method should use a for loop to sum all odd numbers in that range including the end and return the sum.
+It should call the method isOdd to check if each number is odd.
+The parameter end needs to be greater than or equal to start and both start and end parameters have to be greater than 0.
+If those conditions are not satisfied return -1 from the method to indicate invalid input.
+ */
+public class ForLoop_SumOdd {
+    public static void main(String[] args) {
+
+    }
+    public static boolean isOdd(int number) {
+        if (number > 0 && number % 2 != 0)
+            return true;
+        else
+            return false;
+    }
+    public static int sumOdd(int start, int end) {
+        int sum = 0;
+        if (end >= start && start > 0 && end > 0) {
+            for (int i = start ; i <= end ; i++) {
+                if (isOdd(i)) {
+                    sum += i;
+                }
+            }
+            return sum;
+        } else
+            return -1;
+    }
+
+}
