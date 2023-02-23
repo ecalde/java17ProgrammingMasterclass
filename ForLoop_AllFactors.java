badb1efa9022e9c0dfe78ec45a0291e2f6bd71f2
Edwin Calderon <calderonedwin3@gmail.com>
2023-02-23 10:27:54 -0600

    Adding ForLoop All Factors Work

---
+++ ForLoop_AllFactors.java
@@ -0,0 +1,24 @@
+/*
+Write a method named printFactors with one parameter of type int named number.
+If number is < 1, the method should print "Invalid Value".
+The method should print all factors of the number. A factor of a number is an integer
+which divides that number wholly (i.e. without leaving a remainder).
+For example, 3 is a factor of 6 because 3 fully divides 6 without
+leaving a remainder. In other words 6 / 3 = 2.
+*/
+public class ForLoop_AllFactors {
+    public static void main(String[] args) {
+
+    }
+    public static void printFactors(int number) {
+        if (number < 1)
+            System.out.println("Invalid Value");
+        else {
+            int temp = number;
+            for (int i = 1; i <= temp ;i++) {
+                if (temp % i == 0)
+                    System.out.print(i + " ");
+            }
+        }
+    }
+}
